00f81c55865a4c2f8e974f55615b1fe958373e5b
NivShirazi <nivshirazi9@gmail.com>
2023-02-15 05:24:46 -0800

    project with details components

--- hackathon/src/app/app.tsx
+++ hackathon/src/app/app.tsx
@@ -6,7 +6,19 @@ export function App() {
   return (
     <div className={styles['app']}>
       <Routes>
-        <Route path="/" element={<Project name="niv" />}></Route>
+        <Route
+          path="/"
+          element={
+            <Project
+              authorName="ניב שיראזי"
+              projName="פרויקט חדשני"
+              date="15/02/23"
+              description='לורם איפסום או בקיצור ליפסום הוא מלל מקובל וחסר משמעות המשמש "ממלא מקום" בעת עריכה, בתחום הדפוס, ההדפסה והפרסום. הטקסט משמש כלי לייצוג דפוס שאמור להתווסף למוצר הפרסום בעתיד.
+               מדובר במלל ארוך במיוחד שניתן לבדוק עמו גופנים,
+               וכן התאמת המלל לעמודים המעוצבים ובדיקת סוגי וגודלי גופן והתאמתם למוצר הסופי.'
+            />
+          }
+        ></Route>
         <Route path="/page1" element={<h1> page 1</h1>}></Route>
         <Route path="/page2" element={<h1> page 2</h1>}></Route>
       </Routes>
--- hackathon/src/app/components/project/project.tsx
+++ hackathon/src/app/components/project/project.tsx
@@ -1,19 +1,53 @@
-import styles from './project.module.css';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-/* eslint-disable-next-line */
 export interface ProjectProps {
-  name: string;
+  authorName: string;
+  projName: string;
+  date: string;
+  description: string;
+  imgUrl?: string;
 }
 
-export function Project(props: ProjectProps) {
+export default function RecipeReviewCard(props: ProjectProps) {
   return (
-    <div className={styles['container']}>
-      <h2 className={styles['component-name']}>Example Component Name</h2>
-      <p className={styles['component-description']}>
-        This is an example component description.
-      </p>
-    </div>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar
+            sx={{ bgcolor: red[500], marginLeft: '10px' }}
+            aria-label="name"
+          >
+            {props.authorName.charAt(0)}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.projName}
+        subheader={props.date}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="assets\download.jpg"
+        alt="project image"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {props.description}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
-
-export default Project;
