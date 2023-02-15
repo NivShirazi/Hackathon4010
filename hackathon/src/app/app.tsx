0f016b5692534e60bf5138de97442c2d6f103d26
NivShirazi <nivshirazi9@gmail.com>
2023-02-15 04:44:27 -0800

    add project component

--- hackathon/src/app/app.tsx
+++ hackathon/src/app/app.tsx
@@ -1,16 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
-import Project from './components/project/project';
 import styles from './app.module.css';
+import PrimarySearchAppBar from './components/appBar/appBar';
+import HomeScreen from './screens/HomeScreen';
 
 export function App() {
   return (
-    <div className={styles['app']}>
-      <Routes>
-        <Route path="/" element={<Project name="niv" />}></Route>
-        <Route path="/page1" element={<h1> page 1</h1>}></Route>
-        <Route path="/page2" element={<h1> page 2</h1>}></Route>
-      </Routes>
-    </div>
+    <>
+      <PrimarySearchAppBar />
+      <div className={styles['app']}>
+        <Routes>
+          <Route path="/" element={<HomeScreen />}></Route>
+          <Route path="/page1" element={<h1> page 1</h1>}></Route>
+          <Route path="/page2" element={<h1> page 2</h1>}></Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
