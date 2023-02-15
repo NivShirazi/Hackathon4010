159e8dda95cae1e98604408c299768757b223a37
NivShirazi <nivshirazi9@gmail.com>
2023-02-15 02:50:56 -0800

    add routes

--- hackathon/src/app/app.tsx
+++ hackathon/src/app/app.tsx
@@ -1,15 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
+import { Route, Routes } from 'react-router-dom';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="hackathon" />
-
-      <div />
-    </>
+    <Routes>
+      <Route path="/" element={<h1> home page</h1>}></Route>
+      <Route path="/page1" element={<h1> page 1</h1>}></Route>
+      <Route path="/page2" element={<h1> page 2</h1>}></Route>
+    </Routes>
   );
 }
 
