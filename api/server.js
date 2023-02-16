d41940582a0280e632c3e6d268b4eb2577b2ac11
NivShirazi <nivshirazi9@gmail.com>
2023-02-15 19:12:48 -0800

    fix token and roni bullshit

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config()
 const cors=require("cors");
 const corsOptions ={
    origin:'*', 
@@ -10,13 +11,15 @@ const app = express();
 app.use(cors(corsOptions))
 
   
-  const accountSid = 'AC252454c41bdd8a7e1e161c9b9c3f5987';
-  const authToken = '8b203217f58c5c8ad02296c256e1b501';
-  const from = 'whatsapp:+17275945568';
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const fromSms = process.env.TWILIO_PHONE_NUMBER_SMS;
+  const fromWA = process.env.TWILIO_PHONE_NUMBER_WHATSAPP;
   const msg = `שלום שגריר יקר,
   נמצאת מתאים לעזור למעיין בתחום העיצוב.
   נשמח אם תוכל לעזור לה ולשפר את תוכנית Lead.
   ליצירת קשר - 0535318117`
+  console.log("id " + accountSid);
   const client = require('twilio')(accountSid, authToken);
   app.get('/', (req, res) => {
     sendSms();
@@ -24,10 +27,10 @@ app.use(cors(corsOptions))
     res.send('Successful response.');
   });
  const sendWhatsapp = () => client.messages
-        .create({body: msg, from: 'whatsapp:+14155238886', to: 'whatsapp:+972509595094'})
+        .create({body: msg, from: fromWA, to: 'whatsapp:+972509595094'})
         .then(message => console.log("send msg " + message.sid));
 
 const sendSms = () => client.messages
-      .create({body: msg, from: '+17275945568', to: '+972509595094'})
+      .create({body: msg, from: fromSms, to: '+972509595094'})
       .then(message => console.log(message.sid));
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
--- hackathon/webpack.config.js
+++ hackathon/webpack.config.js
@@ -4,26 +4,26 @@ const { withReact } = require('@nrwl/react');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
-  module: {
-    rules: [
-      {
-        test: /\.md$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-          {
-            loader: "remark-loader",
-            options: {
-              remarkOptions: {
-                plugins: [RemarkHTML],
-              },
-            },
-          },
-        ],
-      },
-    ],
-  };
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.md$/,
+  //       use: [
+  //         {
+  //           loader: "html-loader",
+  //         },
+  //         {
+  //           loader: "remark-loader",
+  //           options: {
+  //             remarkOptions: {
+  //               plugins: [RemarkHTML],
+  //             },
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // };
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
   return config;
