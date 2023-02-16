a3ff5c3cbdf0070ff3b5489ff6afefb55485eb84
NivShirazi <nivshirazi9@gmail.com>
2023-02-15 21:57:01 -0800

    fix route

--- hackathon/src/app/screens/Home/components/welcome.tsx
+++ hackathon/src/app/screens/Home/components/welcome.tsx
@@ -5,40 +5,78 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import logo from 'C:/Users/Hashlama/Hackathon4010/hackathon/src/assets/icon.jpeg';
+import logo from '../../../../assets/icon.jpeg  ';
 import { useNavigate } from 'react-router-dom';
 
-
-
-
 export default function ImgMediaCard() {
-    const navigate = useNavigate();
-    return (
-    <div style={{backgroundColor: 'black'}}>
-    <Card style={{backgroundColor: 'gray'}}>
-      <CardMedia
-        component="img"
-        alt="logo"
-        style={{ width: 300, height: 300, borderRadius: '20%', position : "relative" , right: "40%" }}
-        
-        image={logo}
-
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h4" align="center" color="#ffffff" component="div" style={{ width: '100%' , right: "40%"}}> 
-          ברוכים הבאים!
-        </Typography>
-        <Typography variant="inherit" align="center" color="#ffffff" style={{ width: '100%' , right: "40%"}}>
-          מנהיגים יוצרים מנהיגים
-          <br></br>
-          בואו לעזור לדורות העתיד ולקחת חלק ברקמה אנושית אחת שמטרתה לקדם את הקהילה
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button onClick={() => navigate('/register')} size="large" variant="contained" style={{ width: '25%', position : "relative" , right: "15%", backgroundColor:'#B98376'}}>הירשם</Button>
-        <Button onClick={() => navigate('/home')} size="large" variant="contained" style={{ width: '25%', position : "relative" , right: "40%", backgroundColor:'#B98376'}}>המשך כאורח</Button>
-      </CardActions>
-    </Card>
+  const navigate = useNavigate();
+  return (
+    <div style={{ backgroundColor: 'black' }}>
+      <Card style={{ backgroundColor: 'gray' }}>
+        <CardMedia
+          component="img"
+          alt="logo"
+          style={{
+            width: 300,
+            height: 300,
+            borderRadius: '20%',
+            position: 'relative',
+            right: '40%',
+          }}
+          image={logo}
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h4"
+            align="center"
+            color="#ffffff"
+            component="div"
+            style={{ width: '100%', right: '40%' }}
+          >
+            ברוכים הבאים!
+          </Typography>
+          <Typography
+            variant="inherit"
+            align="center"
+            color="#ffffff"
+            style={{ width: '100%', right: '40%' }}
+          >
+            מנהיגים יוצרים מנהיגים
+            <br></br>
+            בואו לעזור לדורות העתיד ולקחת חלק ברקמה אנושית אחת שמטרתה לקדם את
+            הקהילה
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            onClick={() => navigate('/register')}
+            size="large"
+            variant="contained"
+            style={{
+              width: '25%',
+              position: 'relative',
+              right: '15%',
+              backgroundColor: '#B98376',
+            }}
+          >
+            הירשם
+          </Button>
+          <Button
+            onClick={() => navigate('/home')}
+            size="large"
+            variant="contained"
+            style={{
+              width: '25%',
+              position: 'relative',
+              right: '40%',
+              backgroundColor: '#B98376',
+            }}
+          >
+            המשך כאורח
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
